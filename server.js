c7e5606c7daf65f47483a9030f7bc2360c48b41d
OP_Developer <op1999mina@gmail.com>
2021-12-09 15:49:36 +0530

    port fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import { APP_PORT, DB_URL }  from './config';
 import errorHandler from './middleware/errorHandler';
-const PORT = process.env.PORT || APP_PORT;
+const PORT = process.env.PORT || 3000;
 const app= express();
 import routes from './routes';
 import mongoose from 'mongoose';
